9795e18efc03c0256816e18de706d2f76dd02bb9
iamamolsarker <amolsarker17@gmail.com>
2025-06-16 21:36:21 +0600

    my bookings complete

--- src/Pages/Event User/MyBookingList.jsx
+++ src/Pages/Event User/MyBookingList.jsx
@@ -4,6 +4,9 @@ import BookingListView from "./BookingListView";
 import BookingGridView from "./BookingGridView";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Lottie from "lottie-react";
+import noDataFound from "../../assets/animation/no_data_found.json"
+import { Link } from "react-router";
 
 const MyBookingList = ({ myBookingsPromise }) => {
   const initialBookingData = use(myBookingsPromise);
@@ -22,22 +25,24 @@ const MyBookingList = ({ myBookingsPromise }) => {
       confirmButtonColor: "#ff0000",
       cancelButtonColor: "#020202",
       confirmButtonText: "Yes, cancle it!",
-      cancelButtonText: "Don't cancle it!"
+      cancelButtonText: "Don't cancle it!",
     }).then((result) => {
       if (result.isConfirmed) {
         axios
           .delete(`http://localhost:5000/event-bookings/${eventId}`)
           .then((res) => {
-            if(res.data.deletedCount){
+            if (res.data.deletedCount) {
               Swal.fire({
-              title: "Cancled!",
-              text: "Your booking has been cancled.",
-              icon: "success",
-              confirmButtonColor: "orange"
-            });
+                title: "Cancled!",
+                text: "Your booking has been cancled.",
+                icon: "success",
+                confirmButtonColor: "orange",
+              });
 
-            const remainingEvent = myBookings.filter(booking => booking._id !== eventId )
-            setMyBookings(remainingEvent);
+              const remainingEvent = myBookings.filter(
+                (booking) => booking._id !== eventId
+              );
+              setMyBookings(remainingEvent);
             }
           })
           .catch((error) => {
@@ -48,26 +53,36 @@ const MyBookingList = ({ myBookingsPromise }) => {
   };
   return (
     <>
-      <div className="max-w-[1300px] mx-auto my-14">
-        <div className="flex justify-end mb-6">
-          <button
-            className="border border-gray-200 rounded-lg p-2 cursor-pointer"
-            onClick={handleView}
-          >
-            {gridView ? <CiBoxList size="25px" /> : <CiGrid41 size="25px" />}
-          </button>
+      {myBookings.length ? (
+        <div className="max-w-[1300px] mx-auto my-14">
+          <div className="flex justify-end mb-6">
+            <button
+              className="border border-gray-200 rounded-lg p-2 cursor-pointer"
+              onClick={handleView}
+            >
+              {gridView ? <CiBoxList size="25px" /> : <CiGrid41 size="25px" />}
+            </button>
+          </div>
+          <div>
+            {gridView ? (
+              <BookingGridView
+                myBookings={myBookings}
+                handleDelete={handleDelete}
+              ></BookingGridView>
+            ) : (
+              <BookingListView
+                myBookings={myBookings}
+                handleDelete={handleDelete}
+              ></BookingListView>
+            )}
+          </div>
         </div>
-        <div>
-          {gridView ? (
-            <BookingGridView myBookings={myBookings} handleDelete={handleDelete}></BookingGridView>
-          ) : (
-            <BookingListView
-              myBookings={myBookings}
-              handleDelete={handleDelete}
-            ></BookingListView>
-          )}
+      ) : (
+        <div className="max-w-[400px] mx-auto my-14 text-center">
+          <Lottie animationData={noDataFound} loop={true} />
+          <Link className="border border-orange-500 text-lg text-orange-500 font-semibold px-4 py-1.5 rounded-xl inline-block mt-6" to="/all-event">See All Event</Link>
         </div>
-      </div>
+      )}
     </>
   );
 };
