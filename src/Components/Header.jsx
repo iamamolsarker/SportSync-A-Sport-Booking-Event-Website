71e1343b5c4b2fbdd20eed0b95051a79716c620f
iamamolsarker <amolsarker17@gmail.com>
2025-08-09 16:17:11 +0600

    navbar alinged with rest of the page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -33,68 +33,75 @@ const Header = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 shadow-sm z-50 relative">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="navbar bg-base-100 shadow-sm z-50 sticky top-0 block">
+      <div className="max-w-[1300px] mx-auto flex justify-between">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 text-lg font-medium rounded-box z-1 mt-3 w-52 p-2 shadow space-y-4"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              {navLinks}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 text-lg font-medium rounded-box z-1 mt-3 w-52 p-2 shadow space-y-4"
+          <Link
+            to="/"
+            className="text-2xl font-extrabold tracking-wider text-transparent bg-gradient-to-r from-orange-500 to-red-600 bg-clip-text"
           >
+            SportSync
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal text-lg font-medium space-x-6 [&>a]:px-2 [&>a]:py-0.5 [&>a.active]:bg-gradient-to-r [&>a.active]:from-orange-500 [&>a.active]:to-red-600 [&>a.active]:text-white [&>a]:rounded">
             {navLinks}
           </ul>
         </div>
-        <Link
-          to="/"
-          className="text-2xl font-extrabold tracking-wider text-transparent bg-gradient-to-r from-orange-500 to-red-600 bg-clip-text"
-        >
-          SportSync
-        </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal text-lg font-medium space-x-6 [&>a]:px-2 [&>a]:py-0.5 [&>a.active]:bg-gradient-to-r [&>a.active]:from-orange-500 [&>a.active]:to-red-600 [&>a.active]:text-white [&>a]:rounded">
-          {navLinks}
-        </ul>
-      </div>
-      <div className="navbar-end">
-        {user ? (
-          <>
-          <img src={user.photoURL} alt="" data-tooltip-id="profile-img-tooltip" data-tooltip-content={`${user.displayName}`} className="w-[55px] h-[55px] object-contain mr-1 border border-gray-200 rounded-full"/>
-          <Link
-            onClick={handleLogout}
-            className="btn bg-gradient-to-r from-orange-500 to-red-600  text-white text-lg"
-          >
-            Logout
-          </Link>
-          </>
-        ) : (
-          <Link
-            to="/login"
-            className="btn bg-gradient-to-r from-orange-500 to-red-600 text-white text-lg"
-          >
-            Login
-          </Link>
-        )}
+        <div className="navbar-end">
+          {user ? (
+            <>
+              <img
+                src={user.photoURL}
+                alt=""
+                data-tooltip-id="profile-img-tooltip"
+                data-tooltip-content={`${user.displayName}`}
+                className="w-[55px] h-[55px] object-contain mr-1 border border-gray-200 rounded-full"
+              />
+              <Link
+                onClick={handleLogout}
+                className="btn bg-gradient-to-r from-orange-500 to-red-600  text-white text-lg"
+              >
+                Logout
+              </Link>
+            </>
+          ) : (
+            <Link
+              to="/login"
+              className="btn bg-gradient-to-r from-orange-500 to-red-600 text-white text-lg"
+            >
+              Login
+            </Link>
+          )}
+        </div>
       </div>
       <Tooltip id="profile-img-tooltip"></Tooltip>
     </div>
-    
   );
 };
 
